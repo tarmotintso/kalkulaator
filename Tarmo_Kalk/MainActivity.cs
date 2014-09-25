0b934299c83a3a69402bb7424cc35d79461f66f2
tarmo <tarmo.tintso@khk.ee>
2014-09-25 20:05:11 +0300

    * MainActivity.cs: Add basic layout : id names, buttons, editbox
    * Main.axml:
    * Resource.designer.cs:

--- Tarmo_Kalk/MainActivity.cs
+++ Tarmo_Kalk/MainActivity.cs
@@ -9,24 +9,16 @@ using Android.OS;
 
 namespace Tarmo_Kalk
 {
-	[Activity (Label = "Tarmo_Kalk", MainLauncher = true, Icon = "@drawable/icon")]
+	[Activity (Label = "Tarmo Kalkulaator", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
 
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 
-			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
 			};
 		}
 	}
